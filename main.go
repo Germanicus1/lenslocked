e8868f867e1da037f66b4f3d241d8e55325a4e35
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-03-20 09:39:28 +0100

    Second commit

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site again!</h1>")
 }
 
 func main() {
