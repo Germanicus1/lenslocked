3154a784bd8abc7f98170597776038cec4f73120
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-03-20 18:26:50 +0100

    Added handler for contact page and page not found.

--- main.go
+++ main.go
@@ -5,13 +5,34 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact</h1><p>To get in touch email me at <a href=\"mailto:peter.kerschbaumer.es\">peter@kerschbaumer.es</a>.")
+}
+
+func pathHandler(w http.ResponseWriter, r *http.Request){
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w,r)
+	case "/contact":
+		contactHandler(w,r)
+	default:
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	}
+}
+
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
+	http.HandleFunc("/contact", pathHandler)
+
 	fmt.Println("Starting the server at :3000...")
 	http.ListenAndServe(":3000", nil)
 }
+
+
