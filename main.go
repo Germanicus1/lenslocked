0e6c0233ac6992df2973041fcc4cde705626519d
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-03-21 19:05:58 +0100

    Recreated the routes with Chi

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	fmt.Fprint(w, "<h1>Welcome to my awesome site again!</h1>")
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact</h1><p>To get in touch email me at <a href=\"mailto:peter.kerschbaumer.es\">peter@kerschbaumer.es</a>.")
+	fmt.Fprint(w, "<h1>Contact Me</h1><p>To get in touch email me at <a href=\"mailto:peter.kerschbaumer.es\">peter@kerschbaumer.es</a>.")
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,28 +33,31 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
-type Router struct{}
+// type Router struct{}
 
-func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w,r)
-	case "/contact":
-		contactHandler(w,r)
-	case "/faq":
-		faqHandler(w,r)
-	default:
-		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-	}
-}
+// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w,r)
+// 	case "/contact":
+// 		contactHandler(w,r)
+// 	case "/faq":
+// 		faqHandler(w,r)
+// 	default:
+// 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+// 	}
+// }
 
 func main() {
-	var router Router
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
-	http.HandleFunc("/faq", contactHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+	})
 
 	fmt.Println("Starting the server at :3000...")
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
 
