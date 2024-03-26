b95defbae8ff01cf843aa73eb79c51ff0ddb5204
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-03-26 19:55:19 +0100

    Added dynamic templating for FAQ

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"githubn.com/Germanicus1/lenslocked/views"
@@ -9,5 +10,28 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request){
 		tpl.Execute(w, nil)
+	}
+}
+
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct{
+		Question string
+		Answer template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer: "Yes, we offer a 30 day trial",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer: "We have support stuff answering email 24/7",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer: `Email us at <a href="mailto:peter.kerschbaumer.es">peter@kerschbaumer.es</a>`,
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request){
+		tpl.Execute(w, questions)
 	}
 }
--- main.go
+++ main.go
@@ -12,17 +12,17 @@ import (
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "home.gohtml"))))
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
-	r.Get("/contact", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "contact.gohtml"))))
+	r.Get("/contact", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "faq.gohtml"))))
+	r.Get("/faq", controllers.FAQ(
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
-	r.Get("/about", controllers.StaticHandler(views.Must(
-		views.ParseFS(templates.FS, "about.gohtml"))))
+	r.Get("/about", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "about.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
