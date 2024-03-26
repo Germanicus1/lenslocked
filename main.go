61967bc60e8b667bb4dc0c85906d538750cb42ca
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-03-26 18:47:58 +0100

    Embeded templates and added support for parsing embeded templates

--- main.go
+++ main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"githubn.com/Germanicus1/lenslocked/controllers"
+	"githubn.com/Germanicus1/lenslocked/templates"
 	"githubn.com/Germanicus1/lenslocked/views"
 )
 
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", controllers.StaticHandler(views.Must(
-		views.Parse(filepath.Join("templates", "home.gohtml")))))
+		views.ParseFS(templates.FS, "home.gohtml"))))
 
 	r.Get("/contact", controllers.StaticHandler(views.Must(
-		views.Parse(filepath.Join("templates", "contact.gohtml")))))
+		views.ParseFS(templates.FS, "contact.gohtml"))))
 
 	r.Get("/faq", controllers.StaticHandler(views.Must(
-		views.Parse(filepath.Join("templates", "faq.gohtml")))))
+		views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.Get("/about", controllers.StaticHandler(views.Must(
-		views.Parse(filepath.Join("templates", "aboutError.gohtml")))))
+		views.ParseFS(templates.FS, "about.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
