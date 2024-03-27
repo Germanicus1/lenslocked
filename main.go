d24d362d43f65461ad008b252e801a785dd177d8
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-03-27 10:19:38 +0100

    Added a tailwind CSS layout

--- main.go
+++ main.go
@@ -13,16 +13,16 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "home.gohtml", "layout-parts.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "home.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml", "tailwind.gohtml"))))
 
 	r.Get("/about", controllers.StaticHandler(
-		views.Must(views.ParseFS(templates.FS, "about.gohtml"))))
+		views.Must(views.ParseFS(templates.FS, "about.gohtml", "tailwind.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
