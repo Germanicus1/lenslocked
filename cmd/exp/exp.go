50d6e679c72463c3d38ff3f600261422ec0219c3
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-04-08 13:00:45 +0200

    Created postgres config code for development.

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,77 +1,47 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"githubn.com/Germanicus1/lenslocked/models"
 )
 
-type PostgresConfig struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Database string
-	SSLMode  string
-}
-
-func (cfg PostgresConfig) String() string {
-	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
-}
-
 func main() {
-	cfg := PostgresConfig{
-		Host:     "localhost",
-		Port:     "5432",
-		User:     "baloo",
-		Password: "junglebook",
-		Database: "lenslocked",
-		SSLMode:  "disable",
-	}
-	db, err := sql.Open("pgx", cfg.String())
+	cfg := models.DefaultPostgresConfig()
+	db, err := models.Open(cfg)
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
 		fmt.Println("Could not ping().")
 		panic(err)
 	}
-	defer db.Close()
 	fmt.Println("Connected!")
 
-	_, err = db.Exec(`
-		CREATE TABLE IF NOT EXISTS users (
-			id SERIAL PRIMARY KEY,
-			name TEXT,
-			email TEXT UNIQUE NOT NULL
-		);
-
-		CREATE TABLE IF NOT EXISTS orders (
-			id SERIAL PRIMARY KEY,
-			user_id INT NOT NULL,
-			amount INT,
-			escription TEXT
-		);
-	`)
+	us := models.UserService{
+		DB: db,
+	}
+	user, err := us.Create("peter@kerschbaumer.es", "peter123")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Tables created!")
+	fmt.Println(user)
 
-	name := "Peter"
-	email := "peter1@kerschbaumer.es"
+	// name := "Peter"
+	// email := "peter1@kerschbaumer.es"
 
-	// Insert some data
-	row := db.QueryRow(`
-		INSERT INTO users (name, email)
-		VALUES ($1, $2) RETURNING id`, name, email)
-	var id int
-	err = row.Scan(&id)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("User created. ID:", id)
+	// // Insert some data
+	// row := db.QueryRow(`
+	// 	INSERT INTO users (name, email)
+	// 	VALUES ($1, $2) RETURNING id`, name, email)
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("User created. ID:", id)
 }
