0f9f805ed9cfe7925c57f1016a3f1427cb4552ce
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-04-08 19:01:55 +0200

    Finnished Create() with error handling

--- controllers/users.go
+++ controllers/users.go
@@ -23,6 +23,13 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Email:", r.FormValue("email"))
-	fmt.Fprint(w, "Password:", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Somethiong went wrong.", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User created: %+v", user) // +v gives fieldnames as well
 }
