47d7dc427e794b378cbe98c9da674e8b9a835217
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-04-11 11:02:42 +0200

    Processing the sign in of users

--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func main() {
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
+	r.Post("/signin", usersC.ProcessSignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
