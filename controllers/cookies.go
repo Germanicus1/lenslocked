8095c30941f77096508de327ec189b7f76333f9b
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-04-17 10:07:04 +0200

    added helper functions for creating and setting cookies

---
+++ controllers/cookies.go
@@ -0,0 +1,22 @@
+package controllers
+
+import "net/http"
+
+const (
+	CookieSession = "session"
+)
+
+func newCookie(name, value string) *http.Cookie {
+	cookie := http.Cookie{
+		Name:     name,
+		Value:    value,
+		Path:     "/",
+		HttpOnly: true,
+	}
+	return &cookie
+}
+
+func setCookie(w http.ResponseWriter, name, value string) {
+	cookie := newCookie(name, value)
+	http.SetCookie(w, cookie)
+}
