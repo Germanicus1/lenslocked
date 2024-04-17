63aef653aaefb714650b77cb2aaf30cb6e6e293a
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-04-17 12:26:54 +0200

    refactored rand and put session related code in session models pkg

--- models/session.go
+++ models/session.go
@@ -7,19 +7,35 @@ import (
 	"githubn.com/Germanicus1/lenslocked/rand"
 )
 
+const (
+	MinBytesPerToken = 32
+)
+
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is only set when creating a new session. When look up session
+	// this will be left empty, as we only store the hash of a session token
+	// in our database andwe cannot reverse it into a raw token
 	Token     string
 	TokenHash string
 }
 
 type SessionService struct {
 	DB *sql.DB
+	// BytesPerToken is use to determine how many bytes to use when generating
+	// each session token. If this value is not set or is less than the
+	// MinBytesPerToken const it will be ignored and MinBytesPerToken will be
+	// used.
+	BytesPerToken int
 }
 
 func (ss *SessionService) Create(userID int) (*Session, error) {
-	token, err := rand.SessionToken()
+	bytesPerToken := ss.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
 	if err != nil {
 		return nil, fmt.Errorf("create: %w", err)
 	}
