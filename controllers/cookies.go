bffc3cdf305bb7e57537bd21d905900b72ab4b3b
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-04-19 12:54:42 +0200

    created signout handler, delete cookies and signout router

--- controllers/cookies.go
+++ controllers/cookies.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 const (
 	CookieSession = "session"
@@ -18,5 +21,19 @@ func newCookie(name, value string) *http.Cookie {
 
 func setCookie(w http.ResponseWriter, name, value string) {
 	cookie := newCookie(name, value)
+	http.SetCookie(w, cookie)
+}
+
+func readCookie(r *http.Request, name string) (string, error) {
+	c, err := r.Cookie(name)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", name, err)
+	}
+	return c.Value, nil
+}
+
+func deleteCookie(w http.ResponseWriter, name string) {
+	cookie := newCookie(name, "")
+	cookie.MaxAge = -1
 	http.SetCookie(w, cookie)
 }
