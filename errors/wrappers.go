d9b6f5f8baa41554c573cb1ba33347c6881cfebb
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-09-05 01:01:56 +0200

    addeed a wrapper for AS and IS from the std lib errors pkg

--- errors/wrappers.go
+++ errors/wrappers.go
@@ -1,13 +1,12 @@
-// Package errors provides wrappers for functions in the std lib errors package.
+// Description: Wrappers for the errors package.
 package errors
 
 import "errors"
 
-// As is a variable that gives access to the As function in the std lib errors
-// package.
-var As = errors.As
-
-// Is is a variable that gives access to the Is function in the std lib errors
-// package.
-var Is = errors.Is
+// As and Is are wrappers for the errors package. They let us access the As and
+// Is functions from the errors package.
+var (
+	As = errors.As
+	Is = errors.Is
+)
 
