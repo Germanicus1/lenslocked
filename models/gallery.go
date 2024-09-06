f6961bf37bbed912c9e8ca57d6846d5a66f712fc
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-09-06 14:06:26 +0200

    Creted service to insert galleries into the DB

--- models/gallery.go
+++ models/gallery.go
@@ -1,7 +1,32 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Gallery struct {
 	ID     int
 	UserID int
 	Title  string
+}
+
+type GalleryService struct {
+	DB *sql.DB
+}
+
+func (service *GalleryService) Create(title string, userId int) (*Gallery, error){
+	gallery := Gallery{
+		Title: title,
+		UserID: userId,
+	}
+	row:= service.DB.QueryRow(
+		`INSERT INTO galleries (title, user_id)
+		VALUES ($1, $2) RETURNING id;`,
+		gallery.Title, gallery.UserID)
+	err := row.Scan(&gallery.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create gallery: error creating gallery: %v", err)
+	}
+	return &gallery, nil
 }
