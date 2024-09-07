8ba57ae6e9ffb1caa15dc4b61a64af162e5c88f3
Peter Kerschbaumer <peter@kerschbaumer.se>
2024-09-07 17:31:16 +0200

    Added the handler for creating a new gallery into the controller package

---
+++ controllers/galleries.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"net/http"
+
+	"githubn.com/Germanicus1/lenslocked/models"
+)
+
+type Galleries struct {
+	Templates struct {
+		New Template
+	}
+	GalleryService *models.GalleryService
+}
+
+func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Title string
+	}
+	data.Title = r.FormValue("title")
+	g.Templates.New.Execute(w, r, data)
+}
